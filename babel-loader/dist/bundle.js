fb44eee111aa35eef51c0aaeb11eb6a77b7811b6
Raul Anaya <ranaya@qualisys.mx>
2018-01-25 14:34:00 -0700

    Babel loader

---
+++ babel-loader/dist/bundle.js
@@ -0,0 +1,155 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+__webpack_require__(1);
+
+var _message = __webpack_require__(2);
+
+var _message2 = _interopRequireDefault(_message);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+document.write("Probando Web Pack!");
+document.write("<br/>");
+document.write(_message2.default.firstMessage);
+(0, _message.lastMessage)();
+
+console.log("Probando Webpack!");
+
+/***/ }),
+/* 1 */
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
+
+/***/ }),
+/* 2 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+var _renderToDom = __webpack_require__(3);
+
+var _renderToDom2 = _interopRequireDefault(_renderToDom);
+
+var _makeMessage = __webpack_require__(4);
+
+var _makeMessage2 = _interopRequireDefault(_makeMessage);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var waitTime = new Promise(function (good, bad) {
+    setTimeout(function () {
+        good("La operación se ha realizado correctamente.");
+    }, 1000);
+});
+
+module.exports = {
+    firstMessage: "Probando los modulos",
+    lastMessage: async function lastMessage() {
+        var message = await waitTime;
+        (0, _renderToDom2.default)((0, _makeMessage2.default)(message));
+    }
+};
+
+/***/ }),
+/* 3 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+module.exports = function renderToDom(element) {
+    document.body.appendChild(element);
+};
+
+/***/ }),
+/* 4 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+function makeMessage(message) {
+    var element = document.createElement("p");
+    element.textContent = message;
+    return element;
+}
+
+exports.default = makeMessage;
+
+/***/ })
+/******/ ]);
---
+++ babel-loader/index.js
@@ -0,0 +1,11 @@
+import "./estilos.css";
+import message from "./message.js";
+import { lastMessage} from "./message.js";
+
+document.write("Probando Web Pack!");
+document.write("<br/>")
+document.write(message.firstMessage);
+lastMessage();
+
+console.log("Probando Webpack!");
+
